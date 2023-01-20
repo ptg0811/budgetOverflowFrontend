698bcd70bc800a36c370fec238ea7305dcb97efb
ptg0811 <ptg0811@gmail.com>
2023-01-20 10:40:23 +0900

    FEAT: login test

--- src/components/users/signup/NaverSignupButton.tsx
+++ src/components/users/signup/NaverSignupButton.tsx
@@ -8,6 +8,7 @@ import NaverLogo from '../../../assets/icons/ico_Naver_logo.png';
 
 // TODO: media query 설정
 // TODO: redirect uri, client id env 파일 설정
+// TODO: test
 const NaverSignupButton = () => {
   // TODO: state 추후 변경 필요
   const naverStateString = 'test';
